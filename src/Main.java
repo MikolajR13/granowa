8f0728efd48c322408c868f16b44fcdf1eedb322
Wiktor Stankiewicz <wiktorstankiewicz24@gmail.com>
2022-05-27 16:48:01 +0200

    added "XD"

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        new Controller();System.out.println("Aaaaaaaaaaaaaaa");
+        new Controller();
+        System.out.println("Aaaaaaaaaaaaaaa");
+        System.out.println("XD");
     }
 }
